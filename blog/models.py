4f59c008f22d49de5d083e8c7285b7d750a61fdf
Анастасия Сапунова < >
2019-10-17 00:21:49 +0300

    Добавление изменений из презентации Семинар-5

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,33 @@
 from django.conf import settings
 from django.db import models
-from django.urls import reverse
 from django.utils import timezone
 
-# Create your models here.
+from .managers import PostManager, PostPublishedManager
+
+
 class Post(models.Model):
-	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	title = models.CharField(max_length=200)
-	text = models.TextField()
-	created_date = models.DateTimeField(default=timezone.now)
-	published_date = models.DateTimeField(blank=True, null=True)
-	"""docstring for Post"""
-	def get_absolute_url(self):
-		return reverse('post_detail', args=[str(self.id)])
+    """Django data model Post"""
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-	def publish(self):
-		self.published_date = timezone.now()
-		self.save()
+    objects = PostManager()
+    published = PostPublishedManager()
 
-	def __str__(self):
-		return self.title
+    def is_publish(self):
+        return True if self.published_date else False
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    class Meta:
+        verbose_name = 'Запись в блоге'
+        verbose_name_plural = 'Записи в блоге'
+
+    def __str__(self):
+        return self.title
 
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,36 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.utils import timezone
-
+from django.shortcuts import get_object_or_404, render_to_response
+from .forms import PostForm
 from .models import Post
 
 # Create your views here.
+def post_add(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return render(request, 'blog/post_edit.html', {'form': form})
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
+
 def post_list(request):
-	posts = Post.objects.filter(
-		published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.for_user(user=request.user)
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, id):
-	post = Post.objects.get(id=id)
+	post = get_object_or_404(Post, id=id)
+	if not post.is_publish() and not request.user.is_staff:
+		raise Http404("Запись в блоге не найдена")
 	return render(request, 'blog/post_detail.html', {'post': post})
+
+def handler404(request, exception, template_name="404.html"):
+    response = render_to_response("404.html")
+    response.status_code = 404
+    return response
+	
